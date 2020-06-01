ebb006713376edfa22afd9c98cc898fa444394a5
iMatty <32849750+iMatty@users.noreply.github.com>
2020-06-01 16:39:47 +0200

    created corpus

--- scripts/script.R
+++ scripts/script.R
@@ -1,4 +1,5 @@
 #Ladowanie bibliotek
+library(tm)
 
 #zmiana katalogu roboczego
 workDir <- "C:\\Users\\mwmat\\Desktop\\PJN-Projekt28"
@@ -8,3 +9,20 @@ setwd(workDir)
 inputDir <- ".\\data"
 outputDir <- ".\\results"
 dir.create(outputDir, showWarnings = FALSE)
+
+#utworzenie korpusu dokumentow
+corpusDir <- paste(
+  inputDir,
+  "tematy - oryginal",
+  sep = "\\"
+)
+corpus <- VCorpus(
+  DirSource(
+    corpusDir,
+    pattern = "*.txt",
+    encoding = "UTF-8"
+  ),
+  readerControl = list(
+    language = "pl_PL"
+  )
+)
