b7d13ee48fa469900c3ececb848e031d1fb8b920
iMatty <32849750+iMatty@users.noreply.github.com>
2020-06-02 12:16:27 +0200

    added features

--- scripts/script.R
+++ scripts/script.R
@@ -2,6 +2,13 @@
 library(tm)
 library(hunspell)
 library(stringr)
+library(lsa)
+library(topicmodels)
+library(wordcloud)
+library(proxy)
+library(dendextend)
+library(corrplot)
+library(flexclust)
 
 #zmiana katalogu roboczego
 workDir <- "C:\\Users\\mwmat\\Desktop\\PJN-Projekt28"
@@ -122,7 +129,7 @@ tdmTfBounds <- TermDocumentMatrix(
   corpus, 
   control = list(
     bounds = list(
-      global = c(2,16)
+      global = c(1,20)
     )
   )
 )
@@ -131,7 +138,7 @@ tdmTfidfBounds <- TermDocumentMatrix(
   control = list(
     weighting = weightTfIdf,
     bounds = list(
-      global = c(2,16)
+      global = c(1,20)
     )
   )
 )
@@ -147,7 +154,7 @@ dtmTfidfBounds <- DocumentTermMatrix(
   control = list(
     weighting = weightTfIdf,
     bounds = list(
-      global = c(2,16)
+      global = c(1,20)
     )
   )
 )
@@ -238,3 +245,305 @@ legend(
   text.col = "orange"
 )
 dev.off()
+
+################################################################################################ lsa.R
+#analiza ukrytych wymiarów semantycznych (dekompozycja wg. wartoœci osobliwych)
+lsa <- lsa(tdmTfidfBoundsMatrix)
+lsa$tk #odpowiednik macierzy U, wspó³rzêdne wyrazów
+lsa$dk #odpowiednik macierzy V, wspó³rzêdne dokumentów
+lsa$sk #odpowiednik macierzy D, znaczenie sk³adowych
+
+#przygotowanie danych do wykresu
+coordTerms <- lsa$tk%*%diag(lsa$sk)
+coorDocs <- lsa$dk%*%diag(lsa$sk)
+terms <- c("") #tu trzeba s³owa klucze
+#(harry", "czarodziej", "dumbledore", "hermiona", "ron", "komnata", "powiedzieæ", "chcieæ", "dowiadywaæ", "albus", "syriusz", "lupin", "umbridge", "edmund", "kaspian", "³ucja", "czarownica", "piotr", "zuzanna", "aslana", "narnii", "baron", "dziecko", "wyspa", "bell", "edward", "wampir", "jacob"
+termsImportance <- diag(lsa$tk%*%diag(lsa$sk)%*%t(diag(lsa$sk))%*%t(lsa$tk))
+importantTerms <- names(tail(sort(termsImportance),25))
+coordTerms <- coordTerms[terms,]
+#coordTerms <- coordTerms[importantTerms,]
+legend <- paste(paste("d", 1:19, sep = ""), rownames(coorDocs), sep = "<-")
+x1 <- coorDocs[,1]
+y1 <- coorDocs[,2]
+x2 <- coordTerms[,1]
+y2 <- coordTerms[,2]
+
+#wykres dokumentów i wybranych s³ów w przestrzeni dwuwymiatowej
+options(scipen = 5)
+plot(
+  x1, 
+  y1, 
+  xlim = c(-0.2,0.05),
+  #ylim = c(,),
+  pch = 1, 
+  col = "orange"
+)
+points(
+  x2, 
+  y2, 
+  pch = 2, 
+  col = "brown"
+)
+text(
+  x1, 
+  y1, 
+  paste("d", 1:19, sep = ""), 
+  col = "orange",
+  pos = 4
+)
+text(
+  x2, 
+  y2, 
+  rownames(coordTerms), 
+  col = "brown",
+  pos = 4
+)
+legend("bottomleft", legend, cex = 0.7, text.col = "orange")
+
+#eksport wykresu do pliku .png
+plotFile <- paste(
+  outputDir, 
+  "lsa.png",
+  sep = "\\"
+)
+png(file = plotFile)
+options(scipen = 5)
+plot(
+  x1, 
+  y1, 
+  xlim = c(-0.2,0.05),
+  #ylim = c(,),
+  pch = 1, 
+  col = "orange"
+)
+points(
+  x2, 
+  y2, 
+  pch = 2, 
+  col = "brown"
+)
+text(
+  x1, 
+  y1, 
+  paste("d", 1:19, sep = ""), 
+  col = "orange",
+  pos = 4
+)
+text(
+  x2, 
+  y2, 
+  rownames(coordTerms), 
+  col = "brown",
+  pos = 4
+)
+legend("bottomleft", legend, cex = 0.5, text.col = "orange")
+dev.off()
+
+############################################################################################################## Ida.R
+#analiza ukrytej alokacji Dirichlet'a
+nTerms <- ncol(dtmTfAll)
+nTopics <- 4
+lda <- LDA(
+  dtmTfAll,
+  k = nTopics,
+  method = "Gibbs",
+  control = list(
+    burnin = 2000,
+    thin = 100,
+    iter = 3000
+  )
+)
+perplexity <- perplexity(lda, dtmTfAll)
+results <- posterior(lda)
+
+par(mai = c(1,2,1,1))
+#prezentacja tematów
+topic1 <- head(sort(results$terms[1,], decreasing = T),20)
+barplot(
+  rev(topic1),
+  horiz = T,
+  las = 1, 
+  main = "Temat 1",
+  xlab = "Prawdopodobieñstwo",
+  col = "orange"
+)
+
+topic2 <- head(sort(results$terms[2,], decreasing = T),20)
+barplot(
+  rev(topic2),
+  horiz = T,
+  las = 1, 
+  main = "Temat 2",
+  xlab = "Prawdopodobieñstwo",
+  col = "violet"
+)
+
+topic3 <- head(sort(results$terms[3,], decreasing = T),20)
+barplot(
+  rev(topic3),
+  horiz = T,
+  las = 1, 
+  main = "Temat 3",
+  xlab = "Prawdopodobieñstwo",
+  col = "turquoise"
+)
+
+topic4 <- head(sort(results$terms[4,], decreasing = T),20)
+barplot(
+  rev(topic4),
+  horiz = T,
+  las = 1, 
+  main = "Temat 4",
+  xlab = "Prawdopodobieñstwo",
+  col = "lightgreen"
+)
+
+#prezentacja dokumentów
+document1 <- results$topics[1,]
+barplot(
+  rev(document1),
+  horiz = T,
+  las = 1, 
+  main = rownames(results$topics)[1],
+  xlab = "Prawdopodobieñstwo",
+  col = "violet"
+)
+
+document4 <- results$topics[4,]
+barplot(
+  rev(document4),
+  horiz = T,
+  las = 1, 
+  main = rownames(results$topics)[4],
+  xlab = "Prawdopodobieñstwo",
+  col = "lightgreen"
+)
+
+document11 <- results$topics[11,]
+barplot(
+  rev(document11),
+  horiz = T,
+  las = 1, 
+  main = rownames(results$topics)[11],
+  xlab = "Prawdopodobieñstwo",
+  col = "turquoise"
+)
+
+document19 <- results$topics[19,]
+barplot(
+  rev(document19),
+  horiz = T,
+  las = 1, 
+  main = rownames(results$topics)[19],
+  xlab = "Prawdopodobieñstwo",
+  col = "orange"
+)
+
+#################################################################################### keywords.R
+#dla pierwszego dokumentu
+##wagi tf jako miara wa¿noœci s³ów
+keywordsTf1 <- head(sort(dtmTfAllMatrix[1,], decreasing = T))
+keywordsTf1
+
+##wagi tfidf jako miara wa¿noœci s³ów
+keywordsTfidf1 <- head(sort(dtmTfidfAllMatrix[1,], decreasing = T))
+keywordsTfidf1
+
+##lda jako miara wa¿noœci s³ów
+termsImportance1 <- c(results$topics[1,]%*%results$terms)
+names(termsImportance1) <- colnames(results$terms)
+keywordsLda1 <- head(sort(termsImportance1, decreasing = T))
+keywordsLda1
+
+##chmura tagów
+par(mai = c(0,0,0,0))
+wordcloud(corpus[16], max.words = 200, colors = brewer.pal(8,"PuOr"))
+
+##################################################################################### clustering.R
+#analiza skupieñ dokumentów
+##hierarchiczna
+#parametry matedy:
+# 1. macierz czêstoœci:
+# a. waga (weighting)
+# b. zakres zmiennych (bounds)
+# 2. miara odleg³oœci (euclideam, jaccard, cosine)
+# 3. sposób wyznaczania odleg³oœci pomiêdzy skupieniami (single, complete, ward.D2)
+
+par(mai = c(1,2,1,1))
+###eksperyment 1
+dist1 <- dist(dtmTfAllMatrix, method = "euclidean")
+hclust1 <- hclust(dist1, method = "ward.D2")
+plot(hclust1)
+barplot(
+  hclust1$height, 
+  names.arg = 18:1, 
+  col = "orange"
+)
+nClusters1 = 5
+clusters1 <- cutree(hclust1, k = nClusters1)
+clustersMatrix1 <- matrix(0, 19, nClusters1)
+rownames(clustersMatrix1) <- names(clusters1)
+for (i in 1:19) {
+  clustersMatrix1[i,clusters1[i]] <- 1
+}
+corrplot(clustersMatrix1)
+dendrogram1 <- as.dendrogram(hclust1)
+coloredDendrogram1 <- color_branches(dendrogram1, h = 100)
+plot(coloredDendrogram1)
+
+###eksperyment 2
+dist2 <- dist(dtmTfidfBoundsMatrix, method = "cosine")
+hclust2 <- hclust(dist2, method = "ward.D2")
+plot(hclust2)
+barplot(
+  hclust2$height, 
+  names.arg = 18:1, 
+  col = "orange"
+)
+nClusters2 = 3
+clusters2 <- cutree(hclust2, k = nClusters2)
+clustersMatrix2 <- matrix(0, 19, nClusters2)
+rownames(clustersMatrix2) <- names(clusters2)
+for (i in 1:19) {
+  clustersMatrix2[i,clusters2[i]] <- 1
+}
+corrplot(clustersMatrix2)
+dendrogram2 <- as.dendrogram(hclust2)
+coloredDendrogram2 <- color_branches(dendrogram2, h = 1.5)
+plot(coloredDendrogram2)
+
+###porównanie wyników eksperymentów
+Bk_plot(
+  dendrogram1,
+  dendrogram2,
+  add_E = F,
+  rejection_line_asymptotic = F,
+  main = "Index Fawlks'a Mallows'a",
+  ylab = "Index Fawlks'a Mallows'a"
+)
+
+##niehierarchiczna (k-œrednich)
+#parametry matedy:
+# 1. macierz czêstoœci:
+# a. waga (weighting)
+# b. zakres zmiennych (bounds)
+# 2. zak³¹dana liczba klas
+
+###eksperyment 3
+nClusters3 <- 3
+kmeans3 <- kmeans(dtmTfidfBounds, centers = nClusters3)
+clustersMatrix3 <- matrix(0, 19, nClusters3)
+rownames(clustersMatrix3) <- names(kmeans3$cluster)
+for (i in 1:19) {
+  clustersMatrix3[i,kmeans3$cluster[i]] <- 1
+}
+corrplot(clustersMatrix3)
+
+pattern <- c(1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3)
+
+##porównanie wyników klasyfikacji
+randEx1Ex3 <- randIndex(clusters1, kmeans3$cluster, F)
+randEx1Ex2 <- randIndex(clusters1, clusters2, F)
+
+randEx1Pattern <- randIndex(clusters1, pattern, F)
+randEx2Pattern <- randIndex(clusters2, pattern, F)
