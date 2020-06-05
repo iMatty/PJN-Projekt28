41457ac74739055c9708d3ce5da26ad0d743be5f
iMatty <32849750+iMatty@users.noreply.github.com>
2020-06-05 11:59:22 +0200

    fixed przetwarzanie wstepne problems

--- scripts/28_Kod.R
+++ scripts/28_Kod.R
@@ -41,7 +41,7 @@ corpus <- VCorpus(
   )
 )
 
-#wstepne przetwarzanie
+#wstêpne przetwarzanie
 corpus <- tm_map(corpus, removeNumbers)
 corpus <- tm_map(corpus, removePunctuation)
 corpus <- tm_map(corpus, content_transformer(tolower))
@@ -54,38 +54,42 @@ stoplist <- readLines(stoplistFile, encoding = "UTF-8")
 corpus <- tm_map(corpus, removeWords, stoplist)
 corpus <- tm_map(corpus, stripWhitespace)
 
-#usuniecie z tekstow em dash i 3/4
+#usuniêcie z tekstów em dash i 3/4
 removeChar <- content_transformer(function(text,pattern) gsub(pattern, "", text))
 corpus <- tm_map(corpus, removeChar, intToUtf8(8722))
 corpus <- tm_map(corpus, removeChar, intToUtf8(190))
+corpus <- tm_map(corpus, removeChar, "«")
+corpus <- tm_map(corpus, removeChar, "„")
+corpus <- tm_map(corpus, removeChar, "”")
+corpus <- tm_map(corpus, removeChar, "–")
 
-#usuniecie rozszerzen z nazw dokumentow
+#usuniêcie rozszerzeñ z nazw dokumentów
 cutExtensions <- function(document, extension) {
-  meta(document, "id") <- gsub(paste("\\.", extension, "$", sep=""),"", meta(document, "id"))
+  meta(document, "id") <- gsub(paste("\\.",extension,"$", sep=""),"", meta(document, "id"))
   return(document)
 }
 corpus <- tm_map(corpus, cutExtensions, "txt")
 
-#usuniecie podzialu na akapity z tekstu
+#usuniêcie podzia³u na akapity z tekstu
 pasteParagraphs <- content_transformer(function(text, char) paste(text, collapse = char))
 corpus <- tm_map(corpus, pasteParagraphs, " ")
 
 #lematyzacja
 polish <- dictionary(lang="pl_PL")
-lemmantize <- function(text) {
+lemmatize <- function(text) {
   simpleText <- str_trim(as.character(text))
   vectorizedText <- str_split(simpleText, pattern = " ")
   lemmatizedText <- hunspell_stem(vectorizedText[[1]], dict = polish)
   for (i in 1:length(lemmatizedText)) {
     if (length(lemmatizedText[[i]]) == 0) lemmatizedText[i] <- vectorizedText[[1]][i]
-    if (length(lemmatizedText[[i]]) > 1) lemmatizedText[i] <- lemmatizedText[[i]][1]
+    if (length(lemmatizedText[[i]]) >  1) lemmatizedText[i] <- lemmatizedText[[i]][1]
   }
   newText <- paste(lemmatizedText, collapse = " ")
   return(newText)
 }
-corpus <- tm_map(corpus, content_transformer(lemmantize))
+corpus <- tm_map(corpus, content_transformer(lemmatize))
 
-#eksport korpousu dokumentow wstepnie potrzetworzonych
+#eksport korpusu dokumentów wstêpnie przetworzonych
 preprocessedDir <- paste(
   inputDir,
   "tematy - przetworzone",
@@ -93,6 +97,8 @@ preprocessedDir <- paste(
 )
 dir.create(preprocessedDir)
 writeCorpus(corpus, path = preprocessedDir)
+
+#writeLines(corpus[[1]]$content)
 
 #################################################################################################################################### frequency_matrix.R
 
